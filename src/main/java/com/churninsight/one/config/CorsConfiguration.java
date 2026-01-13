97f4cdcc57d847480cdc85d7bf7d3beaf9db7b97
adrianABD <adriandiaz.abd@gmail.com>
2026-01-13 18:23:06 -0500

    Cambios en el Cors

--- src/main/java/com/churninsight/one/config/CorsConfiguration.java
+++ src/main/java/com/churninsight/one/config/CorsConfiguration.java
@@ -14,10 +14,10 @@ public class CorsConfiguration {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         org.springframework.web.cors.CorsConfiguration configuration = new org.springframework.web.cors.CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://localhost:5500"));
+        configuration.setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:4200"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
-        configuration.setAllowCredentials(true);
+        configuration.setAllowCredentials(false);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
