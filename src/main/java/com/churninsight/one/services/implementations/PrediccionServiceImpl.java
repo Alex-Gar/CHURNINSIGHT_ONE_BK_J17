63eb14b9ca6c7ce2de9208d0d0df66f92085865c
Meyling Rojas <mpatric03@hotmail.com>
2026-01-15 16:43:56 -0600

    Actualización de la lógica en PrediccionServiceImpl

--- src/main/java/com/churninsight/one/services/implementations/PrediccionServiceImpl.java
+++ src/main/java/com/churninsight/one/services/implementations/PrediccionServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
